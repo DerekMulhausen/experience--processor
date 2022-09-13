b6755178a1704a732ddc070e0bdc85f198bc8f3f
dmulhausen <westbycomputers@hotmail.com>
2022-09-12 20:01:45 -0500

    certificates done

--- js/main.js
+++ js/main.js
@@ -6,14 +6,15 @@ xhr.responseText='text';
 
 xhr.onload=function(){
     if(xhr.status===200){
-        var myStuff=JSON.parse(xhr.responseText);
-        console.log(myStuff.certificates);
+        var Experience=JSON.parse(xhr.responseText);
+        console.log(Experience.certificates);
         var skills={};
         skillList=[];
         workList=[];
-        myStuff.certificates.forEach(cert=>{
+        Experience.certificates.forEach(cert=>{
             //create cert block for skills
             let certData={
+                "id":cert.id,
                 "source":cert.source,
                 "title":cert.title,
                 "url":cert.url
@@ -40,10 +41,45 @@ xhr.onload=function(){
                     skillList.push(dataBlock);
                 }
             });
+            if(concurrentSkills.length>0){
+                skillTime=((100-cert.distinctSillPct)/100)*cert.hours;
+                cert.concurrentSkills.forEach(skill=>{
+                    const skillIndex=skillList.findIndex(elem=>{
+                        return elem.name===skill;
+                    });
+                    if(skillIndex>-1){
+                        skillList[skillIndex].hours+=skillTime;
+                        skillList[skillIndex].certificates.push(certData);
+                    }else{
+                        let dataBlock={
+                            "name":skill,
+                            "hours":skillTime,
+                            "certificates":[certData],
+                            "work":[],
+                            "projects":[],
+                            "courses":[]
+                        };
+                        skillList.push(dataBlock);
+                    }
+                });
+            }
+        });
+        Experience.projects.forEach(proj=>{
+            let projData={
+                "id":proj.id,
+                "title": proj.title,
+                "desc":proj.desc,
+                "type":proj.type,
+                "workid":proj.workid,
+                "completed":proj.completed,
+                "repo":proj.repo,
+                
+            };
         });
         console.log(skillList);
     }
 };
+function updateSkills(skillList, sourceBlock, skillBlock)
 
 xhr.send();
 
