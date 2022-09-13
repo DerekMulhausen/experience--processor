c65223fe8bc200e992111f42ba8a03b0027da3a5
dmulhausen <westbycomputers@hotmail.com>
2022-09-13 16:45:50 -0500

    projects done

--- js/main.js
+++ js/main.js
@@ -22,46 +22,48 @@ xhr.onload=function(){
             //get list of skills
             //calc distinct skills
             let skillTime=(cert.hours*(cert.distinctSkillPct/100))/cert.distinctSkills.length;
-            cert.distinctSkills.forEach(skill=>{
-                const skillIndex=skillList.findIndex(elem=>{
-                    return elem.name===skill;
-                });
-                if(skillIndex>-1){
-                    skillList[skillIndex].hours+=skillTime;
-                    skillList[skillIndex].certificates.push(certData);
-                }else{
-                    let dataBlock={
-                        "name":skill,
-                        "hours":skillTime,
-                        "certificates":[certData],
-                        "work":[],
-                        "projects":[],
-                        "courses":[]
-                    };
-                    skillList.push(dataBlock);
-                }
-            });
-            if(concurrentSkills.length>0){
+            skillList=updateSkills(skillList, certData, cert.distinctSkills, 'certificates', skillTime);
+            // cert.distinctSkills.forEach(skill=>{
+            //     const skillIndex=skillList.findIndex(elem=>{
+            //         return elem.name===skill;
+            //     });
+            //     if(skillIndex>-1){
+            //         skillList[skillIndex].hours+=skillTime;
+            //         skillList[skillIndex].certificates.push(certData);
+            //     }else{
+            //         let dataBlock={
+            //             "name":skill,
+            //             "hours":skillTime,
+            //             "certificates":[certData],
+            //             "work":[],
+            //             "projects":[],
+            //             "courses":[]
+            //         };
+            //         skillList.push(dataBlock);
+            //     }
+            // });
+            if(cert.concurrentSkills.length>0){
                 skillTime=((100-cert.distinctSillPct)/100)*cert.hours;
-                cert.concurrentSkills.forEach(skill=>{
-                    const skillIndex=skillList.findIndex(elem=>{
-                        return elem.name===skill;
-                    });
-                    if(skillIndex>-1){
-                        skillList[skillIndex].hours+=skillTime;
-                        skillList[skillIndex].certificates.push(certData);
-                    }else{
-                        let dataBlock={
-                            "name":skill,
-                            "hours":skillTime,
-                            "certificates":[certData],
-                            "work":[],
-                            "projects":[],
-                            "courses":[]
-                        };
-                        skillList.push(dataBlock);
-                    }
-                });
+                skillList=updateSkills(skillList, certData, cert.concurrentSkills, 'certificates', skillTime);
+                // cert.concurrentSkills.forEach(skill=>{
+                //     const skillIndex=skillList.findIndex(elem=>{
+                //         return elem.name===skill;
+                //     });
+                //     if(skillIndex>-1){
+                //         skillList[skillIndex].hours+=skillTime;
+                //         skillList[skillIndex].certificates.push(certData);
+                //     }else{
+                //         let dataBlock={
+                //             "name":skill,
+                //             "hours":skillTime,
+                //             "certificates":[certData],
+                //             "work":[],
+                //             "projects":[],
+                //             "courses":[]
+                //         };
+                //         skillList.push(dataBlock);
+                //     }
+                // });
             }
         });
         Experience.projects.forEach(proj=>{
@@ -75,11 +77,67 @@ xhr.onload=function(){
                 "repo":proj.repo,
                 
             };
+            const skillTime=proj.hours/proj.skills.length;
+            skillList=updateSkills(skillList, projData, proj.skills, 'projects', skillTime, true);
+            if(proj.workid>0){
+                const workIndex=workList.findIndex(elem=>{
+                    return elem.id===projData.workid;
+                });
+                if(workIndex>-1){
+                    workList[workIndex].projectHours+=proj.hours;
+                }else{
+                    workList.push({
+                        "id":proj.workid,
+                        "projectHours":proj.hours
+                    });
+                }
+            }
+        });
+        Experience.work.forEach(work=>{
+            let workData={
+                "id":work.id,
+                "company":work.company,
+                "title":work.title,
+                "start":work.start,
+                "end":work.end
+            };
+            let dateParse=split(work.start,'-');
+            const dateStart=new Date(dateParse[0],dateParse[1]-1, dateParse[2]);
+            dateParse=split(work.end,'-');
+            const dateEnd=new Date(dateParse[0],dateParse[1]-1, dateParse[2]);
+            const timeDiff=dateEnd.getTime()-dateStart.getTime();
+            const dayDiff=timeDiff/(1000*3600*24);
+            const workHours=dayDiff*5/7*8;
+            
+
         });
         console.log(skillList);
+        console.log(workList);
     }
 };
-function updateSkills(skillList, sourceBlock, skillBlock)
+function updateSkills(skillList, sourceBlock, skillBlock, sourceType, skillTime){
+    skillBlock.forEach(skill=>{
+        const skillIndex=skillList.findIndex(elem=>{
+            return elem.name===skill;
+        });
+        if(skillIndex>-1){
+            skillList[skillIndex].hours+=skillTime;
+            skillList[skillIndex][sourceType].push(sourceBlock);
+        }else{
+            let dataBlock={
+                "name":skill,
+                "hours":skillTime,
+                "certificates":[],
+                "work":[],
+                "projects":[],
+                "courses":[]
+            };
+            dataBlock[sourceType].push(sourceBlock);
+            skillList.push(dataBlock);
+        }
+    });
+    return skillList;
+}
 
 xhr.send();
 
